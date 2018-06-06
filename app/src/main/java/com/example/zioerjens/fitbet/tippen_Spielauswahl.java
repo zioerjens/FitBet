2b95671a7db5c3f48f5d02b723ee3a43efe20c6c
Florian Moser <florian.moser@ict-campus.net>
2018-06-06 15:32:25 +0200

    Tabs leer final

--- app/src/main/java/com/example/zioerjens/fitbet/tippen_Spielauswahl.java
+++ app/src/main/java/com/example/zioerjens/fitbet/tippen_Spielauswahl.java
@@ -38,8 +38,8 @@ public class tippen_Spielauswahl extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tippen__spielauswahl);
 
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+        //Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        //setSupportActionBar(toolbar);
         // Create the adapter that will return a fragment for each of the three
         // primary sections of the activity.
         mSectionsPagerAdapter = new SectionsPagerAdapter(getSupportFragmentManager());

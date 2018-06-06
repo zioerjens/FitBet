6a7f2bbc3386c53b7d918faa1166d95aa1bb1bcb
Florian Moser <florian.moser@ict-campus.net>
2018-06-06 11:35:46 +0200

    Home Activity

--- app/src/main/java/com/example/zioerjens/fitbet/Home.java
+++ app/src/main/java/com/example/zioerjens/fitbet/Home.java
@@ -3,6 +3,8 @@ package com.example.zioerjens.fitbet;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Home extends AppCompatActivity {
 
@@ -10,7 +12,15 @@ public class Home extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
-        Intent intent = new Intent(getApplicationContext(),TestJsonParse.class);
-        startActivity(intent);
+
+
+        final Button button = findViewById(R.id.btnStatistik);
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(),TestJsonParse.class);
+                startActivity(intent);
+            }
+        });
+
     }
 }

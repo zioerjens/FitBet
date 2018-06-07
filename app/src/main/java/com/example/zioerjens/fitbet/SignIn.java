29191c6068cee4aa50d426c2ebe3660193148974
zioerjens <sven.zioerjen@ict-campus.net>
2018-06-07 16:54:10 +0200

    Updated to universal login

--- app/src/main/java/com/example/zioerjens/fitbet/SignIn.java
+++ app/src/main/java/com/example/zioerjens/fitbet/SignIn.java
@@ -1,6 +1,8 @@
 package com.example.zioerjens.fitbet;
 
 import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -13,13 +15,20 @@ import com.google.android.gms.auth.api.signin.GoogleSignInClient;
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
 import com.google.android.gms.common.SignInButton;
 import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthCredential;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.GoogleAuthProvider;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
 public class SignIn extends AppCompatActivity {
 
     private GoogleSignInClient mGoogleSignInClient;
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,24 +38,32 @@ public class SignIn extends AppCompatActivity {
         // Configure sign-in to request the user's ID, email address, and basic
         // profile. ID and basic profile are included in DEFAULT_SIGN_IN.
         GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken(getString(R.string.default_web_client_id))
                 .requestEmail()
                 .build();
 
         // Build a GoogleSignInClient with the options specified by gso.
         mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
+
+        mAuth = FirebaseAuth.getInstance();
     }
 
     @Override
     protected void onStart() {
         super.onStart();
 
+        /*
         // Check for existing Google Sign In account, if the user is already signed in
         // the GoogleSignInAccount will be non-null.
         GoogleSignInAccount account = GoogleSignIn.getLastSignedInAccount(this);
-        updateUI(account);
+        updateUI(account);*/
+
+        // Check if user is signed in (non-null) and update UI accordingly.
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        updateUI(currentUser);
     }
 
-    private void updateUI(GoogleSignInAccount account){
+    private void updateUI(FirebaseUser account){
         //TODO change UI according to the logged in user - if user is not logged in, account returns null
         if (account == null){
             // Set the dimensions of the sign-in button.
@@ -54,12 +71,39 @@ public class SignIn extends AppCompatActivity {
             signInButton.setSize(SignInButton.SIZE_WIDE);
             signInButton.setOnClickListener(new SignInListener(this));
         } else {
+            Account.setAccount(account);
             Toast.makeText(this,"Successfully logged in!", Toast.LENGTH_SHORT).show();
             Intent intent = new Intent(getApplicationContext(),Home.class);
             startActivity(intent);
             finish();
         }
     }
+
+    private void firebaseAuthWithGoogle(GoogleSignInAccount acct) {
+        Log.d("firebaseAuthWithGoogle", "firebaseAuthWithGoogle:" + acct.getId());
+        Log.e("sven","authwithgoogle-!-");
+
+        AuthCredential credential = GoogleAuthProvider.getCredential(acct.getIdToken(), null);
+        mAuth.signInWithCredential(credential)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Log.d("firebaseAuthWithGoogle", "signInWithCredential:success");
+                            FirebaseUser user = mAuth.getCurrentUser();
+                            updateUI(user);
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Log.w("firebaseAuthWithGoogle", "signInWithCredential:failure", task.getException());
+                            updateUI(null);
+                        }
+
+                        // ...
+                    }
+                });
+    }
+
 
     public void signIn(){
         Intent signInIntent = mGoogleSignInClient.getSignInIntent();
@@ -76,14 +120,23 @@ public class SignIn extends AppCompatActivity {
             // a listener. Also, it is only one activity from which you can sign in,
             // that's why the requestCode is hardcoded here.
             Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
-            handleSignInResult(task);
+            try {
+                // Google Sign In was successful, authenticate with Firebase
+                GoogleSignInAccount account = task.getResult(ApiException.class);
+                firebaseAuthWithGoogle(account);
+            } catch (ApiException e) {
+                // Google Sign In failed, update UI appropriately
+                // ...
+            }
+            //handleSignInResult(task);
         }
     }
+
+    /*
 
     private void handleSignInResult(Task<GoogleSignInAccount> completedTask) {
         try {
             GoogleSignInAccount account = completedTask.getResult(ApiException.class);
-            Account.setAccount(account);
             // Signed in successfully, show authenticated UI.
             updateUI(account);
         } catch (ApiException e) {
@@ -93,4 +146,5 @@ public class SignIn extends AppCompatActivity {
             updateUI(null);
         }
     }
+    */
 }

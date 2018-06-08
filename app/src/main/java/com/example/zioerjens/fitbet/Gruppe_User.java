1612d31d9baaf0bf7ac03dc71113a77de38605b5
Florian Moser <florian.moser@ict-campus.net>
2018-06-08 16:37:36 +0200

    Friti aabe. Es geit nid

---
+++ app/src/main/java/com/example/zioerjens/fitbet/Gruppe_User.java
@@ -0,0 +1,11 @@
+package com.example.zioerjens.fitbet;
+
+public class Gruppe_User {
+    public String userID;
+    public String groupID;
+
+    public Gruppe_User(String userID, String groupID){
+        this.userID = userID;
+        this.groupID = groupID;
+    }
+}
--- app/src/main/java/com/example/zioerjens/fitbet/Gruppenauswahl.java
+++ app/src/main/java/com/example/zioerjens/fitbet/Gruppenauswahl.java
@@ -10,17 +10,31 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
 
 public class Gruppenauswahl extends AppCompatActivity {
+
+    DatabaseReference databaseGruppen;
+    ArrayList<Gruppe> listgruppe;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_gruppenauswahl);
 
-        Button newGroup = (Button) findViewById(R.id.btnGrErstellen);
+        databaseGruppen = FirebaseDatabase.getInstance().getReference().child("gruppen");
+
+        listgruppe = new ArrayList<>();
+
+        final Button newGroup = (Button) findViewById(R.id.btnGrErstellen);
         Button joinGroup = (Button) findViewById(R.id.btnGrBeitreten);
         newGroup.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -41,11 +55,24 @@ public class Gruppenauswahl extends AppCompatActivity {
                         FirebaseDatabase database = FirebaseDatabase.getInstance();
                         DatabaseReference ref = database.getReference("gruppe");
                         DatabaseReference usersRef = ref.push();
+
                         Gruppe g = new Gruppe(mName.getText().toString(),mPassword.getText().toString());
                         //usersRef.child("gruppe").setValue(g);
                         usersRef.setValue(g);
-                        //String a=Account.getAccount().getId();
-                        //Log.e("bababdi",a);
+
+                        //Add current user to new Group
+                        DatabaseReference ref2 = database.getReference("gruppe_user");
+                        DatabaseReference usersRef2 = ref2.push();
+                        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+                        String currentUserID = user.getUid();
+
+                        Gruppe_User gu = new Gruppe_User(currentUserID,usersRef.getKey());
+                        usersRef2.setValue(gu);
+
+                        Log.e("CurrentUserId",currentUserID);
+
+
+
                         dialog.dismiss();
 
 
@@ -61,10 +88,77 @@ public class Gruppenauswahl extends AppCompatActivity {
             public void onClick(View v) {
                 AlertDialog.Builder mBuilder = new AlertDialog.Builder(Gruppenauswahl.this);
                 View mView = getLayoutInflater().inflate(R.layout.activity_join_group,null);
+                final EditText mName = (EditText) mView.findViewById(R.id.groupnameJoin);
+                final EditText mPassword = (EditText) mView.findViewById(R.id.passwordJoin);
+                Button mjoinGr = (Button) mView.findViewById(R.id.btnGrBeitreten);
                 mBuilder.setView(mView);
                 final AlertDialog dialog = mBuilder.create();
                 dialog.show();
+
+                mjoinGr.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        for(Gruppe g : listgruppe){
+                            Log.w("Name:",g.name);
+                            if(mName.getText().toString()==g.name){
+                                FirebaseDatabase database = FirebaseDatabase.getInstance();
+                                DatabaseReference ref = database.getReference("gruppe");
+                                DatabaseReference usersRef = ref.child(g.name);
+
+
+                                DatabaseReference ref2 = database.getReference("gruppe_user");
+                                DatabaseReference usersRef2 = ref2.push();
+                                FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+                                String currentUserID = user.getUid();
+
+                                Gruppe_User gu = new Gruppe_User(currentUserID,usersRef.getKey());
+                                usersRef2.setValue(gu);
+                                dialog.dismiss();
+                            }
+                        }
+                    }
+                });
+
             }
         });
+
+
+
+    }
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        /*databaseGruppen.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+
+                listgruppe.clear();
+
+                for(DataSnapshot gruppeSnapshot : dataSnapshot.getChildren()){
+                    Gruppe gruppe = gruppeSnapshot.getValue(Gruppe.class);
+                    listgruppe.add(gruppe);
+                }
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });*/
+
+        ValueEventListener valueEventListener = new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                Gruppe g = dataSnapshot.getValue(Gruppe.class);
+                listgruppe.add(g);
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        };
+        databaseGruppen.addValueEventListener(valueEventListener);
     }
 }

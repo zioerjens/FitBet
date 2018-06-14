c2572c643e215fd5505ca236bd77b00ca83d1e02
Mat Ramseier <matthias.ramseier@ict-campus.net>
2018-06-14 10:05:25 +0200

    removed some unused imports

--- app/src/main/java/com/example/zioerjens/fitbet/Sport.java
+++ app/src/main/java/com/example/zioerjens/fitbet/Sport.java
@@ -17,7 +17,6 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.database.ChildEventListener;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
@@ -26,8 +25,6 @@ import com.google.firebase.database.ValueEventListener;
 
 import java.math.RoundingMode;
 import java.text.DecimalFormat;
-import java.util.HashMap;
-import java.util.Map;
 
 public class Sport extends AppCompatActivity {
 
